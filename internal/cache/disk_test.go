9f4891499f0910e3dabf4b00f076a5595b7f2bab
Alec Thomas <alec@swapoff.org>
2026-01-05 10:55:31 +1100

    refactor: switch the cache API to an object store
    I think this will simplify our lives, vs. supporting arbitrary
    filesystem-like paths.

--- internal/cache/disk_test.go
+++ internal/cache/disk_test.go
@@ -13,6 +13,10 @@ import (
 	"github.com/block/sfptc/internal/logging"
 )
 
+func testKey(s string) cache.Key {
+	return cache.NewKey(s)
+}
+
 func TestDiskStorageExpiry(t *testing.T) {
 	dir := t.TempDir()
 	_, ctx := logging.Configure(t.Context(), logging.Config{Level: slog.LevelDebug})
@@ -22,13 +26,16 @@ func TestDiskStorageExpiry(t *testing.T) {
 
 	assert.Equal(t, int64(0), disk.Size())
 
-	w1, err := disk.Create(ctx, "/first", time.Second*3)
+	key1 := testKey("first")
+	key2 := testKey("second")
+
+	w1, err := disk.Create(ctx, key1, time.Second*3)
 	assert.NoError(t, err)
 	_, err = w1.Write([]byte("first file data"))
 	assert.NoError(t, err)
 	assert.NoError(t, w1.Close())
 
-	w2, err := disk.Create(ctx, "/second", time.Second)
+	w2, err := disk.Create(ctx, key2, time.Second)
 	assert.NoError(t, err)
 	_, err = w2.Write([]byte("second file data"))
 	assert.NoError(t, err)
@@ -39,11 +46,11 @@ func TestDiskStorageExpiry(t *testing.T) {
 
 	time.Sleep(time.Second * 2)
 
-	r, err := disk.Open(ctx, "/first")
+	r, err := disk.Open(ctx, key1)
 	assert.NoError(t, err)
 	assert.NoError(t, r.Close())
 
-	_, err = disk.Open(ctx, "/second")
+	_, err = disk.Open(ctx, key2)
 	assert.IsError(t, err, os.ErrNotExist)
 
 	assert.Equal(t, int64(len("first file data")), disk.Size())
@@ -62,27 +69,32 @@ func TestDiskAsyncEviction(t *testing.T) {
 
 	assert.Equal(t, int64(0), disk.Size(), "initial size should be 0")
 
+	keyExpire1 := testKey("expire1")
+	keyExpire2 := testKey("expire2")
+	keyKeep1 := testKey("keep1")
+	keyKeep2 := testKey("keep2")
+
 	// Create files that will expire
-	w1, err := disk.Create(ctx, "/expire1", 800*time.Millisecond)
+	w1, err := disk.Create(ctx, keyExpire1, 800*time.Millisecond)
 	assert.NoError(t, err)
 	_, err = w1.Write([]byte("expires soon"))
 	assert.NoError(t, err)
 	assert.NoError(t, w1.Close())
 
-	w2, err := disk.Create(ctx, "/expire2", 800*time.Millisecond)
+	w2, err := disk.Create(ctx, keyExpire2, 800*time.Millisecond)
 	assert.NoError(t, err)
 	_, err = w2.Write([]byte("expires soon"))
 	assert.NoError(t, err)
 	assert.NoError(t, w2.Close())
 
 	// Create files that won't expire
-	w3, err := disk.Create(ctx, "/keep1", 10*time.Second)
+	w3, err := disk.Create(ctx, keyKeep1, 10*time.Second)
 	assert.NoError(t, err)
 	_, err = w3.Write([]byte("keep this"))
 	assert.NoError(t, err)
 	assert.NoError(t, w3.Close())
 
-	w4, err := disk.Create(ctx, "/keep2", 10*time.Second)
+	w4, err := disk.Create(ctx, keyKeep2, 10*time.Second)
 	assert.NoError(t, err)
 	_, err = w4.Write([]byte("keep this too"))
 	assert.NoError(t, err)
@@ -95,17 +107,21 @@ func TestDiskAsyncEviction(t *testing.T) {
 	time.Sleep(1500 * time.Millisecond)
 
 	// Check filesystem directly to verify async eviction actually deleted files
-	_, err = os.Stat(filepath.Join(dir, "expire1"))
+	hexExpire1 := keyExpire1.String()
+	_, err = os.Stat(filepath.Join(dir, hexExpire1[:2], hexExpire1))
 	assert.IsError(t, err, os.ErrNotExist, "expire1 should be deleted from disk")
 
-	_, err = os.Stat(filepath.Join(dir, "expire2"))
+	hexExpire2 := keyExpire2.String()
+	_, err = os.Stat(filepath.Join(dir, hexExpire2[:2], hexExpire2))
 	assert.IsError(t, err, os.ErrNotExist, "expire2 should be deleted from disk")
 
 	// Non-expired files should still exist on disk
-	_, err = os.Stat(filepath.Join(dir, "keep1"))
+	hexKeep1 := keyKeep1.String()
+	_, err = os.Stat(filepath.Join(dir, hexKeep1[:2], hexKeep1))
 	assert.NoError(t, err, "keep1 should still exist on disk")
 
-	_, err = os.Stat(filepath.Join(dir, "keep2"))
+	hexKeep2 := keyKeep2.String()
+	_, err = os.Stat(filepath.Join(dir, hexKeep2[:2], hexKeep2))
 	assert.NoError(t, err, "keep2 should still exist on disk")
 
 	// Verify size only includes non-expired files
